7f00e54290c9325dd78aebb0074c0599674f4038
juanmagdev <me@juanmagd.dev>
2023-01-15 10:38:19 +0100

    first commit

---
+++ src/components/AddCategory.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { useState } from 'react'
+
+export const AddCategory = ({ onNewCategory }) => {
+
+  const [inputValue, setInputValue] = useState('');
+
+  const onInputChange = (event) => {
+    setInputValue(event.target.value);
+  }
+
+  const onSubmit = (event) => {
+    event.preventDefault();
+    console.log(inputValue);
+
+    if (inputValue.trim().length <= 1) return;
+    // setCategories(categories => [ inputValue, ...categories]);
+    onNewCategory(inputValue.trim())
+    setInputValue('');
+
+  }
+
+  return (
+    <form onSubmit={onSubmit}>
+      <input
+        type='text'
+        placeholder='Buscar Gifs'
+        value={inputValue}
+        onChange={onInputChange}
+      />
+    </form>
+
+  )
+}
