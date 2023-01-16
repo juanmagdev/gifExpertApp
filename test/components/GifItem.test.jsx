ec6170274fc48718e18d891344bd53ecc950e8f7
juanmagdev <me@juanmagd.dev>
2023-01-16 13:45:51 +0100

    test on getGifs() and <GifItem/>

--- test/components/GifItem.test.jsx
+++ test/components/GifItem.test.jsx
@@ -1,4 +1,4 @@
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import { GifItem } from "../../src/components/GifItem";
 
 describe('Pruebas en <GifItem />', () => {
@@ -10,5 +10,23 @@ describe('Pruebas en <GifItem />', () => {
     const {container} = render(<GifItem title={title} url = {url}/>);
     expect(container).toMatchSnapshot();
   });
+
+  test('Debe de mostrar la imagen con el URL y el alt indicado', () => {
+
+    render(<GifItem title={title} url = {url}/>);
+    // screen.debug();
+    // expect(screen.getByRole('img').src).toBe(url);
+    // expect(screen.getByRole('img').alt).toBe(title);
+    const {src, alt} = screen.getByRole('img');
+    expect(src).toBe(url);
+    expect(alt).toBe(title);
+
+  });
+
+  test('Debe mostrarse el titulo en el componente', () => {
+    render(<GifItem title={title} url = {url}/>);
+    expect(screen.getByText(title)).toBeTruthy();
+  })
+  
   
 })
